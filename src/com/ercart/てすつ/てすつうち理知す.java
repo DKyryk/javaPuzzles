b2294182c18de69c861959f6398093254fae9286
dkyryk <dkyryk@provectus.com>
2017-08-23 21:45:32 +0300

    test japanese identifiers

---
+++ src/com/ercart/てすつ/てすつうち理知す.java
@@ -0,0 +1,17 @@
+package com.ercart.てすつ;
+
+/**
+ * @author dkyryk
+ */
+public class てすつうち理知す {
+
+    public static String コンヴォルト(Long ぬもべる) {
+        return ぬもべる.toString();
+    }
+
+    public static void main(String[] あるぐめんて) {
+        てすつうち理知す インスタんつ = new てすつうち理知す();
+
+        System.out.println(インスタんつ.コンヴォルト(23L));
+    }
+}
